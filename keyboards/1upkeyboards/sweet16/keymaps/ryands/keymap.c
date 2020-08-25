aae39f23f65f5ec63eb7d79621892f24ec4789b3
ryands <ryan@ryands.org>
2020-08-24 17:20:34 -0700

    1upkeyboards/sweet16:ryands - update layout/mappings for current configuration

--- keyboards/1upkeyboards/sweet16/keymaps/ryands/keymap.c
+++ keyboards/1upkeyboards/sweet16/keymaps/ryands/keymap.c
@@ -2,28 +2,44 @@
 
 enum layers {
     base,
-    fn
+    fn,
+    fn2
 };
 
 #define KC_FN MO(fn)
+#define KC_FN2 MO(fn2)
 
 enum custom_keycodes {
   ADB_FINGER = SAFE_RANGE
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* template:
+    [layer] = LAYOUT_ortho_4x4(
+        _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______
+    )
+    */
     [base] = LAYOUT_ortho_4x4(
-        KC_P7, KC_P8  ,   KC_P9  , KC_TRNS   ,
-        KC_P4, KC_P5  ,   KC_P6  , KC_TRNS   ,
-        KC_P1, KC_P2  ,   KC_P3  , ADB_FINGER,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_FN     
+        KC_P7 , KC_P8  ,   KC_P9  , _______   ,
+        KC_P4 , KC_P5  ,   KC_P6  , _______   ,
+        KC_P1 , KC_P2  ,   KC_P3  , ADB_FINGER,
+        KC_FN2, KC_P0  ,   KC_PDOT, KC_FN     
     ),
     [fn] = LAYOUT_ortho_4x4(
-        KC_F14 , KC_F15 , KC_F16  , KC_F17  ,
-        KC_F18 , KC_F19 , KC_F20  , KC_F21  ,
-        KC_TRNS, KC_UP  , KC_TRNS , KC_TRNS ,
-        KC_LEFT, KC_DOWN, KC_RIGHT, KC_TRNS
+        _______ , KC_UP   , _______ , _______ ,
+        KC_LEFT , _______ , KC_RIGHT, _______ ,
+        _______ , KC_DOWN , _______ , _______ ,
+        _______ , _______ , _______ , _______
     ),
+    [fn2] = LAYOUT_ortho_4x4(
+        _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______ ,
+        _______ , _______ , _______ , _______
+    )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
